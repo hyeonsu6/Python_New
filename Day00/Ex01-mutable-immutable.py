de33b1725aaa0edaea63d063fd6558bb07db79cf
hyeonsu6 <bhshs0508@naver.com>
2024-01-26 17:50:13 +0900

    Day00

---
+++ Day00/Ex01-mutable-immutable.py
@@ -0,0 +1,25 @@
+'''
+Ex01-mutable-immutable.py
+
+mutable - 메모리 값을 변경 가능 객체
+  리스트(list), 세트(set), 딕셔너리(dict), 클래스(class) 등
+
+immtable - 메모리 값 변경 불가
+  정수(int), 실수(float), 문자열(str), 튜플(tuple)
+'''
+
+num1 = 10
+print(id(num1))
+
+# num1 += 1
+# print(id(num1))
+ 
+# num2 = 10
+# print(id(num2))
+
+
+# list1 = [1, 2, 3]
+# print(id(list1))
+# list1.append(4)
+# print(id(list1))
+
---
+++ Day00/hello.py
@@ -0,0 +1,15 @@
+'''
+## git 다시 복습
+
+# git 사용자 등록
+git config --global user.name "username"
+git config --global user.email "이메일주소"
+
+# git clone 명령어
+git clone 깃허브프로젝트주소.git
+'''
+
+print('Hello, World!')
+
+print('Hello, Python!')
+
